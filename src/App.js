2dfe389d8fa0ed926c8e53adf3b7e32211505992
rmikuriya <r_mikuriya@sagin-dp.jp>
2024-10-21 10:20:23 +0900

    auto-deploy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import logo from './logo.svg';
+import './App.css';
 import './App.css';
 
 function App() {
@@ -6,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
